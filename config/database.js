659d426f8ab3c252acb07534ef3b44a6b5d7113c
Hariom851903 <asatihariom95@gmail.com>
2024-04-19 12:39:09 +0530

    change databse.js

--- config/database.js
+++ config/database.js
@@ -4,15 +4,17 @@ require("dotenv").config();
 const { MONGODB_URL } = process.env;
 
 exports.connect = () => {
-	mongoose
-		.connect(MONGODB_URL, {
-			useNewUrlparser: true,
-			useUnifiedTopology: true,
-		})
-		.then(console.log(`DB Connection Success`))
-		.catch((err) => {
-			console.log(`DB Connection Failed`);
-			console.log(err);
-			process.exit(1);
-		});
+    mongoose
+        .connect(MONGODB_URL, {
+            useNewUrlParser: true, // Corrected option name
+            useUnifiedTopology: true,
+        })
+        .then(() => {
+            console.log(`DB Connection Success`);
+        })
+        .catch((err) => {
+            console.log(`DB Connection Failed`);
+            console.error(err);
+            process.exit(1);
+        });
 };
